63894d956098fc0cca511530b3a27fb8d5d1c663
Derick Muluka <mulukaderick@gmail.com>
2024-09-05 02:37:47 +0300

    Fix: Correct active link detection and update navigation menu

--- includes/header.php
+++ includes/header.php
@@ -7,7 +7,8 @@ $current_page = basename($_SERVER['PHP_SELF']);
         <ul>
             <li><a href="index.php" class="<?php echo ($current_page == 'index.php') ? 'active' : ''; ?>">Home</a></li>
             <li><a href="login.php" class="<?php echo ($current_page == 'login.php') ? 'active' : ''; ?>">Login</a></li>
-            <li><a href="register.php" class="<?php echo ($current_page == 'register.php') ? 'active' : ''; ?>">Register</a></li>
+            <li><a href="howitworks.php" class="<?php echo ($current_page == 'register.php') ? 'active' : ''; ?>">How It Works</a></li>
+            <li><a href="aboutus.php" class="<?php echo ($current_page == 'register.php') ? 'active' : ''; ?>">About Us</a></li>
         </ul>
     </nav>
 </header>
